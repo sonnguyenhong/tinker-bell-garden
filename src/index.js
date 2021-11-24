631801dd50bebecc554968320ba442a456c4149b
sonnguyenhong <sonnguyenhong291@gmail.com>
2021-11-24 23:03:48 +0700

    Sua lai cau truc project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP Logger
+app.use(morgan('combined'))
+
+// Template Engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+console.log(path.join(__dirname, 'resources', 'views'))
+
+app.get('/', (req, res, next) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`TinkerBellGarden is listening at http://localhost:${port}`)
+})
