214cfe510ec2930595577426afc0a3b394d3dbd1
๖🆁🅸🅳🅴🆁 ↭ঌ <nam.nt194337@sis.hust.edu.vn>
2021-12-08 14:50:03 +0700

    da sua giao dien

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'resources', 'views'))
 console.log(path.join(__dirname, 'resources', 'views'))
 
 app.get('/', (req, res, next) => {
-    res.render('home')
+    res.render('banhang/trangchu')
 })
 
 app.listen(port, () => {
