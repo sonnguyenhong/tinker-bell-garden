b9845c5d3c1be7ff5316cbaf1e747fde7f8fb612
sonnguyenhong <sonnguyenhong291@gmail.com>
2022-01-15 10:55:05 +0700

    Sua route cua dang ky online. Sua giao dien cua dang ky online cho khach hang

--- src/app/controllers/dangkyonlineController.js
+++ src/app/controllers/dangkyonlineController.js
@@ -109,11 +109,26 @@ class DangkyonlineController {
     index(req, res) {
         sukien.findOne({ startDate: { $lte: Date.now() }, endDate: { $gte: Date.now() } }).lean()
             .then(event => {
-                console.log(event)
-                res.render('home', {
-                    event: event,
-                    hasEvent: true
-                })
+                if (!event) {
+                    const event = {
+                        imageUrl: './img/khu-vui-choi-ha-noi-nemtv-12.jpg',
+                        name: 'Sự kiện đặc biệt',
+                        eventInfo: 'Hiện tại chưa có sự kiện đặc biệt',
+                        numberOfDiscountCode: 0,
+                        currentNumofDiscountCode: 0
+                    }
+                    res.render('home', {
+                        event: event,
+                        hasEvent: false
+                    })
+
+                } else {
+                    console.log(event)
+                    res.render('home', {
+                        event: event,
+                        hasEvent: true
+                    })
+                }
             })
             .catch(err => next(err))
 
