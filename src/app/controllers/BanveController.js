feb2cdc222b3aa98bf3b2e04f0dd403603240b64
sonnguyenhong <sonnguyenhong291@gmail.com>
2022-01-16 17:02:22 +0700

    Sua ban ve: tinh tien

--- src/app/controllers/BanveController.js
+++ src/app/controllers/BanveController.js
@@ -383,9 +383,9 @@ class BanveController {
                 let time = parseInt(req.body.endTime) - parseInt(Date.parse(ticket.startTime))
                 if (ticket.type === 0) {
                     if (time <= 7200000) {
-                        totalPrice = 50000 * time / 3600000
+                        totalPrice = 100000
                     } else {
-                        totalPrice = 50000 * 2 + 70000 * (time - 2 * 3600000) / 3600000
+                        totalPrice = 100000 + 150000 * (time - 2 * 3600000) / 3600000
                     }
                 } else {
                     totalPrice = 200000
--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,9 +1,32 @@
+const Event = require('../models/hoatdong')
 class HomeController {
 
     // [GET] /admin
     index(req, res, next) {
-        console.log('inhere')
-        res.render('admin/admin')
+        Event.findOne({ startDate: { $lte: Date.now() }, endDate: { $gte: Date.now() } }).lean()
+            .then(event => {
+                if (!event) {
+                    const event = {
+                        imageUrl: './img/khu-vui-choi-ha-noi-nemtv-12.jpg',
+                        name: 'Sự kiện đặc biệt',
+                        eventInfo: 'Hiện tại chưa có sự kiện đặc biệt',
+                        numberOfDiscountCode: 0,
+                        currentNumofDiscountCode: 0
+                    }
+                    res.render('admin/admin', {
+                        event: event,
+                        hasEvent: false
+                    })
+
+                } else {
+                    console.log(event)
+                    res.render('admin/admin', {
+                        event: event,
+                        hasEvent: true
+                    })
+                }
+            })
+            .catch(err => next(err))
     }
 
 }
